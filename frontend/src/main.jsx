2cc72e358079fde5e523d1ce23c1c2df41a2fda7
Danwhoosh <133941287+Danwhoosh@users.noreply.github.com>
2025-04-01 14:25:58 +0530

    Add files via upload

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 import App from './App.jsx'
-import './index.css';
-import { AuthProvider } from './auth/context/Authcontext.jsx';
 
 createRoot(document.getElementById('root')).render(
-<AuthProvider>
-<BrowserRouter>
+  <StrictMode>
     <App />
-</BrowserRouter>
-</AuthProvider>
+  </StrictMode>,
 )
